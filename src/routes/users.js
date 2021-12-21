4656d19fe317df922e3bbd9ebe4fb743347be5f5
indjar <indre.bacvinkaite@gmail.com>
2021-12-21 20:51:05 +0200

    fixed bug

--- src/routes/users.js
+++ src/routes/users.js
@@ -20,11 +20,11 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-    const {name, lastname, email, password} = req.body;
+    const {full_name, email, password} = req.body;
     const hashed = await hash(password, 10);
 
     try {
-        const user = await User.create({name, lastname, email, password: hashed});
+        const user = await User.create({full_name, email, password: hashed});
 
         res.status(201).send({
             user,
@@ -62,7 +62,7 @@ router.post("/login", async (req, res) => {
 
         const token = jwt.sign(
             {
-                lecturerId: user.id,
+                acountId: user.id,
             },
             process.env.TOKEN_SECRET
         );
